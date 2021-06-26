6a6ad4134d2b24f63cc1cc19afcd617475983e46
centence <86509730+centence@users.noreply.github.com>
2021-06-26 14:07:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import { run } from "./src/main.js";
 
-const DEFAULT_STEP_SIZE = "5000-15000";
+const DEFAULT_STEP_SIZE = "15000-20000";
+const DEFAULT_XIAOMI_AMAZFIT_USERNAME = "15523432021"
+const DEFAULT_XIAOMI_AMAZFIT_PASSWORD = "gq200606"
 
 // 获取环境变量
 const config = {
-  username: process.env.XIAOMI_AMAZFIT_USERNAME,
-  password: process.env.XIAOMI_AMAZFIT_PASSWORD,
-  user_id: process.env.XIAOMI_AMAZFIT_USER_ID,
-  app_token: process.env.XIAOMI_AMAZFIT_APP_TOKEN,
-  step_size: process.env.STED_SIZE_RANGE ?? DEFAULT_STEP_SIZE,
+  username: DEFAULT_XIAOMI_AMAZFIT_USERNAME ,
+  password: DEFAULT_XIAOMI_AMAZFIT_PASSWORD ,
+  user_id:  "",
+  app_token: "",
+  step_size:  DEFAULT_STEP_SIZE,
 };
 
 await run(config);
